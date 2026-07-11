4ccf4239f92f7bf6936c0dd4c3276c2037ebd3a7
Sandesh Singh <sandesh247@gmail.com>
2026-07-10 19:48:24 -0700

    Bump versionCode to 2 and versionName to 1.0.1 for Google Play Console upload

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.sandesh247.tvvc"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
         buildConfigField("String", "WEB_APP_URL", "\"https://gh-tvvc.web.app\"")
     }
 
